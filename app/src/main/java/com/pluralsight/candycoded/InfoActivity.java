275d39f7aaa4c649d4380639cf4dea0fcf3389f6
Joseph Mbera <josephmbera124@gmail.com>
2019-11-02 15:33:03 +0300

    start phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -34,7 +34,8 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view){
+        Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "0123456789"));
+        startActivity(i);
+    }
 }
